c9aeda7a8a838b98c4f5a73f6cea34a099f88c4f
Tilde Ann Thurium <tilde@thuryism.net>
2019-05-30 16:42:11 -0700

    the phone call works, woohoo

--- call.js
+++ call.js
@@ -4,8 +4,19 @@ const authToken = process.env.TWILIO_PRIMARY_TOKEN
 
 const client = require('twilio')(accountSid, authToken);
 
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
+
+
+// const response = new VoiceResponse();
+// response.play({}, 'https://en.wikipedia.org/wiki/File:Lady_Gaga_-_Telephone.ogg');
+//
+// console.log(response.toString());
+// const number = ''
+
+let numbers;
 client.messages.list({ to: '+15104625981', pageSize: 1000})
 .then(response => {
-  let numbers = new Set(response.map(message => message.from))
-  console.log('numbers', numbers)
+  numbers = new Set(response.map(message => message.from))
+  console.log('numbers', numbers);
+  numbers.forEach(number => client.calls.create({to: number, from: '+15104625981', url: 'http://demo.twilio.com/docs/voice.xml'}))
 });
