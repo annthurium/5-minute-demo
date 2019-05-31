2c970f2b40bf94e90c6ff8352b3b7ebe41c16edf
Tilde Ann Thurium <tilde@thuryism.net>
2019-05-31 10:30:13 -0700

    cleanup

--- call.js
+++ call.js
@@ -1,6 +1,6 @@
 
 const accountSid = process.env.TWILIO_PRIMARY_SID;
-const authToken = process.env.TWILIO_PRIMARY_TOKEN
+const authToken = process.env.TWILIO_PRIMARY_TOKEN;
 
 const client = require('twilio')(accountSid, authToken);
 
@@ -8,6 +8,5 @@ const client = require('twilio')(accountSid, authToken);
 client.messages.list({ to: '+15104625981', pageSize: 1000})
 .then(response => {
   let numbers = new Set(response.map(message => message.from))
-  console.log('numbers', numbers);
   numbers.forEach(number => client.calls.create({to: number, from: '+15104625981', url: 'http://thurium.ngrok.io/voice'}))
 });
